9eccc164787b1e8b868eb36ea9cf3dfbe7217db6
kedi1991 <okurut.henry@yahoo.com>
2023-05-21 03:00:41 +0000

    Created a Seats Model

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'available'), (1, 'reserved'))
+
+
+class Seat(models.Model):
+    seat_id = models.IntegerField(unique=True, null=False)
+    num_of_seats = models.IntegerField(unique=False, null=False)
+    seat_cost = models.IntegerField(unique=False, null=False)
+    seat_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['seat_id']
+    
+    def __str__(self):
+        return self.seat_id
