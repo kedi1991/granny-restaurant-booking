2221b4d34d5593cb68de09d253d327b599a0236d
kedi1991 <okurut.henry@yahoo.com>
2023-12-03 23:13:59 +0000

    Edit the refernce to the booking id

--- reservation/views.py
+++ reservation/views.py
@@ -92,7 +92,7 @@ class EditMyReservations(View):
         Displays the reservation to be edited.
         """
         model = Booking
-        context = {'form':model.objects.filter(id=booking)}
+        context = {'form':model.objects.filter(id=booking_id)}
         template_name = 'edit_booking.html'
 
         return render(request, 'edit_booking.html', context)
