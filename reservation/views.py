cb86d340f7812b8615bbbeb83a59e2899861f1c5
kedi1991 <okurut.henry@yahoo.com>
2023-12-04 00:16:54 +0000

    Import the redirect fucntion to the Views

--- reservation/views.py
+++ reservation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from .models import Seat, Booking
 from .forms import BookingForm
