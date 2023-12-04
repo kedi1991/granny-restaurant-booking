577b1a14e55c8381e208caf9310affc32ee4678a
kedi1991 <okurut.henry@yahoo.com>
2023-12-04 23:28:14 +0000

    Add pass test case for seat creation

--- reservation/tests_models.py
+++ reservation/tests_models.py
@@ -7,7 +7,8 @@ class TestModels(TestCase):
         """
         Create the Seat objects for testing
         """
-        self.seat0 = Seat.objects.create(seat_code='T001', seat_desc='Seat 0 created', seat_persons=3, seats_max=4)    
+        self.seat0 = Seat.objects.create(seat_code='T001', seat_desc='Seat 0 created', seat_persons=3, seats_max=4)
+        self.seat1 = Seat.objects.create(seat_code='T002', seat_desc='Seat 1 created', seat_persons=2, seats_max=3)
 
     def test_seat_create(self):
         self.assertEqual(Seat.objects.count(), 2)
