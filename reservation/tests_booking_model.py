d76741bdf88fceb3cb2f9cd674f07a8bc3742620
kedi1991 <okurut.henry@yahoo.com>
2023-12-04 23:44:19 +0000

    Add pass test case for the Booking creation

--- reservation/tests_booking_model.py
+++ reservation/tests_booking_model.py
@@ -10,4 +10,4 @@ class TestBookingModel(TestCase):
         self.Booking0 = Booking.objects.create(booking_code='T001', booking_client_name='henry', booking_client_phone='077445555', booking_client_email='henry@gmail.com', booking_date='2023-12-12 00:00')
 
     def test_booking_create(self):
-        self.assertEqual(Booking.objects.count(), 2)
+        self.assertEqual(Booking.objects.count(), 1)
